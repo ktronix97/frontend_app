7685ec333358f41d3f20e43e8e1c3861952fe671
ktronix97 <daliaestefany.1701421354@ucaldas.edu.co>
2020-07-21 21:01:06 -0500

    agregue los componentes de la master-page

--- Angular/red-socialNG/src/app/app.module.ts
+++ Angular/red-socialNG/src/app/app.module.ts
@@ -1,16 +1,32 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { RouterModule } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
+import { NavbarComponent } from './public/master-page/navbar/navbar.component';
+import { SideMenuComponent } from './public/master-page/side-menu/side-menu.component';
+import { FooterComponent } from './public/master-page/footer/footer.component';
+import { HeroComponent } from './public/master-page/hero/hero.component';
+
+
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DefaultComponent,
+    NavbarComponent,
+    SideMenuComponent,
+    FooterComponent,
+    HeroComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
