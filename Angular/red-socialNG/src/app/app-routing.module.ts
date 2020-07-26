979bc8cbb03a196dfdedaf95724382e413b7b63b
Eduardo J. Mejia A <eduardo.1701821269@ucaldas.edu.co>
2020-07-25 23:11:13 -0500

    Hero y Navbar creados
    Termine la creacion del Hero y la Navbar, aun falta el register debido a
    la ausencia del modulo seguridad.
    
    El boton "Menu" es de tipo "<a>" por si tienen algun problema para hacer
    que funcione con la sidebar desplegable a crear.

--- Angular/red-socialNG/src/app/app-routing.module.ts
+++ Angular/red-socialNG/src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'home',
+    component: DefaultComponent
+  },
+  {
+    path: '',
+    pathMatch: 'full',
+    redirectTo: '/home'
+  },
+  /*esta opcion va siempre de ultima*/
+  {
+    path: '**',
+    redirectTo: '/home'
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
