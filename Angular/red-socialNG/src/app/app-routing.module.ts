2f2c8b78936ab80b951c321acd8ec2a232e66e72
ktronix97 <daliaestefany.1701421354@ucaldas.edu.co>
2020-07-26 21:02:42 -0500

    Footer completo, side-menu 1/2

--- Angular/red-socialNG/src/app/app-routing.module.ts
+++ Angular/red-socialNG/src/app/app-routing.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DefaultComponent } from './public/home/default/default.component';
 
-const routes: Routes = [
-  {
-    path:'home',
-    component: DefaultComponent
-  },
-  {
-    path: '',
-    pathMatch: 'full',
-    redirectTo: '/home'
-  },
-  /*esta opcion va siempre de ultima*/
-  {
-    path: '**',
-    redirectTo: '/home'
-  }
+const routes: Routes = [ 
+   {
+  path:'home',
+  component: DefaultComponent
+},
+{
+  path: '',
+  pathMatch: 'full',
+  redirectTo: '/home'
+},
+/*esta opcion va siempre de ultima*/
+{
+  path: '**',
+  redirectTo: '/home'
+}
 ];
 
 @NgModule({
