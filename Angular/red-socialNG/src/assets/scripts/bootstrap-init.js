111f1d49adc6d74dff44571ccb7d8763798bc0ca
ktronix97 <daliaestefany.1701421354@ucaldas.edu.co>
2020-07-27 04:06:28 -0500

    side-menu casi completado , faltan algunos ajustes en el css , desarrollo de Login 1/2

--- Angular/red-socialNG/src/assets/scripts/bootstrap-init.js
+++ Angular/red-socialNG/src/assets/scripts/bootstrap-init.js
@@ -1,4 +1,4 @@
-$(function() {
+$(document).ready(function() {
     $('[data-toggle="tooltip"]').tooltip();
     $(".side-nav .collapse").on("hide.bs.collapse", function() {
         $(this).prev().find(".fa").eq(1).removeClass("fa-angle-right").addClass("fa-angle-down");
